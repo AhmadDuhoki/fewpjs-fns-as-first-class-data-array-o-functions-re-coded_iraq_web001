f33d9359c1cb9abaaf85fb52d9b725f686d7be09
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-16 06:23:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
+  let message = `Wake ${dogName} the ${dogBreed}`;
+  return message;
 }
 
 function leashDog(dogName, dogBreed) {
