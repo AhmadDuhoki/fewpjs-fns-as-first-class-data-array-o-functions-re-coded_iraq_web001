0cfeee0c1809196eb3a7b3b2206c16f8be8e011c
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-16 06:25:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,20 +6,30 @@ function wakeDog(dogName, dogBreed) {
 
 function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
+  let message = `Leash ${dogName} the ${dogBreed}`;
+  return message;
 }
 
 function walkDog(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  let message = `Walk to the park with ${dogName} the ${dogBreed}`;
+  return message;
 }
 
 function playWithDog(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  let message = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  return message;
 }
 
 function walkHomeDog(dogName, dogBreed) {
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  let message = `Walk home with ${dogName} the ${dogBreed}`;
+  return message;
 }
 
 function unleashDog(dogName, dogBreed) {
   console.log(`Unleash ${dogName} the ${dogBreed}`);
+  let message = `Unleash ${dogName} the ${dogBreed}`;
+  return message;
 }
